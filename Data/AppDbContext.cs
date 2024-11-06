fbdda926b582910303bf62d5176d853dc3e44a24
aletsita <alexagastelum577@gmail.com>
2024-11-06 08:15:59 -0700

    Agregar archivos de proyecto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Libreria.Data.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Libreria.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Data/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Libreria.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Descripcion { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateTime { get; set; }
+        public int? Rate { get; set; }
+        public string Genero { get; set; }
+        public string Autor { get; set; }
+        public string CoverUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
